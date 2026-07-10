28458a7689670373d94e75cd11d780c2e39d5812
lela-akiashvili <l_akiashvili@cu.edu.ge>
2026-07-10 14:12:50 +0400

    Redo the project cuz of settings

---
+++ app/src/main/java/app/heavenlypillar/geolab/ProductDetailsScreen.kt
@@ -0,0 +1,17 @@
+package app.heavenlypillar.geolab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.*
+
+@Composable
+fun ProductDetailsScreen(){
+  Column
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun ProductDetailsScreenPreview(){
+    ProductDetailsScreen()
+}
